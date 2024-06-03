543f80d309990192632833d2101244bd4c364f86
royj <roy.janco@ceva-dsp.com>
2024-06-03 12:18:37 +0300

    Added configs

--- evaluate_models.py
+++ evaluate_models.py
@@ -12,21 +12,31 @@ from utils import evaluate, get_calibration_loader
 
 if __name__ == '__main__':
     model_dir = 'meta-llama/Llama-2-7b-hf'
+    seq_len = 2048
     print('Initializing tokenizer')
     tokenizer = LlamaTokenizer.from_pretrained(model_dir)
 
     # config_list contains the model that you want to compare. You can remove or add configurations to this list.
     config_list = [
         # 'float',
-        # 'configs/smoothquant_w8a8_per_channel_per_channel_dynamic.yaml',
-        # 'configs/smoothquant_w8a8_per_channel_per_tensor_matmul_dynamic.yaml',
-        'configs/smoothquant_w8a8_per_channel_per_tensor_matmul_static.yaml',
-        'configs/w4a8_per_channel_per_channel_dynamic.yaml',
-        'configs/w4a8_per_channel_per_tensor_matmul_dynamic.yaml',
-        'configs/w8a8_per_channel_per_channel_dynamic.yaml',
-        'configs/w8a8_per_channel_per_tensor_matmul_dynamic.yaml',
-        'configs/w8a8_per_channel_per_tensor_matmul_static.yaml',
-        'configs/w8a8_per_tensor_per_channel_dynamic.yaml'
+        # 'configs/W4A8/w4a8_per_channel_per_channel_matmul_A_dynamic.yaml',
+        # 'configs/W4A8/w4a8_per_channel_per_channel_matmul_A_dynamic.yaml',
+        # 'configs/W4A8/w4a8_per_channel_per_tensor_matmul_B_dynamic.yaml',
+        # 'configs/W4A8/w4a8_per_channel_per_tensor_matmul_C_static.yaml',
+        # 'configs/W4A8/smoothquant_w4a8_per_channel_per_channel_matmul_A_dynamic.yaml',
+        # 'configs/W4A8/smoothquant_w4a8_per_channel_per_tensor_matmul_B_dynamic.yaml',
+        # 'configs/W4A8/smoothquant_w4a8_per_channel_per_tensor_matmul_C_static.yaml',
+        # 'configs/W4A8/smoothquant_a0p4_w4a8_per_channel_per_channel_matmul_A_dynamic.yaml',
+
+        # 'configs/w4a8_per_channel_per_channel_matmul_A_dynamic.yaml',
+        # 'configs/w4a8_per_channel_per_tensor_matmul_B_dynamic.yaml',
+        # 'configs/smoothquant_w8a8_per_channel_per_channel_matmul_A_dynamic.yaml',
+        # 'configs/smoothquant_w8a8_per_channel_per_tensor_matmul_B_dynamic.yaml',
+        # 'configs/smoothquant_w8a8_per_channel_per_tensor_matmul_C_static.yaml',
+        # 'configs/w8a8_per_channel_per_channel_matmul_A_dynamic.yaml',
+        # 'configs/w8a8_per_channel_per_tensor_matmul_B_dynamic.yaml',
+        'configs/w8a8_per_channel_per_tensor_matmul_C_static.yaml',
+        # 'configs/w8a8_per_tensor_per_channel_matmul_B_dynamic.yaml'
     ]
     ppl_list = []
     for config_name in config_list:
@@ -40,7 +50,7 @@ if __name__ == '__main__':
                     conf["SmoothQuant"]["decoder_class"] = LlamaDecoderLayer
                 if conf['QAT']['data_quantization']['quantization_mode'] == 'static':
                     # Add calibration loader
-                    calib_loader = get_calibration_loader(tokenizer, seq_len=2048)
+                    calib_loader = get_calibration_loader(tokenizer, seq_len=seq_len)
                     conf["QAT"]["data_quantization"][
                         "calibration_loader"
                     ] = calib_loader
@@ -49,7 +59,7 @@ if __name__ == '__main__':
                     ] = lambda model, x: model(x.cuda())
                 model = RetrainerModel(model, config=RetrainerConfig(conf))
 
-            ppl = evaluate(model, tokenizer, seq_len=2048)
+            ppl = evaluate(model, tokenizer, seq_len=seq_len)
             print(f'Model {config_name}')
             print(f'Perplexity: {ppl:.4}')
             ppl_list.append({'name': config_name, 'perplexity': ppl.item()})
