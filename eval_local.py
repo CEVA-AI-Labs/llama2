c16d1fc020aefa3235e7217aedf24669d83bb026
royj <roy.janco@ceva-dsp.com>
2025-05-12 15:29:47 +0300

    changed tasks

--- eval_local.py
+++ eval_local.py
@@ -10,7 +10,7 @@ from lm_eval.models.huggingface import HFLM
 import torch
 import json
 
-task_name = "mmlu_flan_n_shot_generative_stem"  # hellaswag  #  mmlu_flan_n_shot_generative_stem
+task_name = ["arc_easy", "hellaswag", "piqa", "openbookqa", "winogrande"]  # arc_easy arc_challenge? hellaswag piqa siqa? openbookqa winogrande
 model_id = "meta-llama/Llama-2-7b-hf"
 # config_file = "configs/w8a8_per_tensor_per_token_dynamic.yaml"
 config_file = "float"
@@ -32,7 +32,7 @@ with torch.no_grad():
 
 hf_model = HFLM(model)
 
-results = simple_evaluate(hf_model, tasks=[task_name], batch_size=8, limit=0.1)
+results = simple_evaluate(hf_model, tasks=task_name, batch_size=8, limit=0.1)
 
 pprint(results['results'])
 
